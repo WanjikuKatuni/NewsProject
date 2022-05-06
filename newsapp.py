d63dff8a86bd5caea4181e9afa86ed26880ac52f
wanjiku <annritawanjiku77@gmail.com>
2022-05-06 07:13:06 +0300

    add news source

--- newsapp.py
+++ newsapp.py
@@ -1,2 +1,10 @@
 from flask import Flask 
 from newsapi impot NewsApiClient
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    newsapi = NewsApiClient(api_key="206c52b0288342de8f6d76f653647450")
+    top_headlines = newsapi.get_top_headlines(sources='bbc-news', language='en', country='us')
