69372c11358344cc04e98d56a69ac1a03db37f7a
wanjiku <annritawanjiku77@gmail.com>
2022-05-06 07:31:14 +0300

    fetch details from news api

--- newsapp.py
+++ newsapp.py
@@ -4,7 +4,7 @@ from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #create url path/method
 def index():
     newsapi = NewsApiClient(api_key="206c52b0288342de8f6d76f653647450")
     top_headlines = newsapi.get_top_headlines(sources='bbc-news', language='en', country='us')
@@ -12,12 +12,20 @@ def index():
     articles = top_headlines['articles']
 
     #add lists to append the data
-    desc = []
+    description = []
     news = []
-    img = []
+    image = []
+    datepublished = []
+    bywho = []
 
 
     for i in range(len(articles)):
         newsarticles = [articles[i]]
 
+        #fetching information
         news.append(newsarticles['title']) #fetch title
+        datepublished.append(newsarticles['publishedAt']) #fetch date published
+        description.append(newsarticles['description']) # fetch description
+        image.append(newsarticles['urlToImage']) #fetchimages
+        bywho.append(newsarticles['author']) #fetch author details
+        
